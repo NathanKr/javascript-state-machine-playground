e9ad688bafd0868b4ba19b648e83337c14055253
Nathan Krasney <natankrasney@gmail.com>
2022-11-13 10:36:22 +0200

    use data

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const StateMachine = require('javascript-state-machine');
+const StateMachine = require("javascript-state-machine");
 
 var fsm = new StateMachine({
-    init: 'solid',
-    transitions: [
-      { name: 'melt',     from: 'solid',  to: 'liquid' },
-      { name: 'freeze',   from: 'liquid', to: 'solid'  },
-      { name: 'vaporize', from: 'liquid', to: 'gas'    },
-      { name: 'condense', from: 'gas',    to: 'liquid' }
-    ],
-    methods: {
-      onMelt:     function() { console.log('I melted')    },
-      onFreeze:   function() { console.log('I froze')     },
-      onVaporize: function() { console.log('I vaporized') },
-      onCondense: function() { console.log('I condensed') }
-    }
-  });
+  init: "solid",
+  data: {
+    color: "red",
+  },
+  methods: {
+    describe: function () {
+      console.log("I am " + this.color);
+    },
+  },
+  transitions: [
+    { name: "melt", from: "solid", to: "liquid" },
+    { name: "freeze", from: "liquid", to: "solid" },
+    { name: "vaporize", from: "liquid", to: "gas" },
+    { name: "condense", from: "gas", to: "liquid" },
+  ],
+  methods: {
+    onMelt: function () {
+      console.log(`I melted with color : ${this.color}`);
+    },
+    onFreeze: function () {
+      console.log("I froze");
+    },
+    onVaporize: function () {
+      console.log("I vaporized");
+    },
+    onCondense: function () {
+      console.log("I condensed");
+    },
+  },
+});
 
 console.log(fsm.state);
-fsm.melt()
+fsm.melt();
 console.log(fsm.state);
